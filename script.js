120289d83d94d8fb5cfb3705b987ef7b943ce533
adityap5 <aditya29pippal@gmail.com>
2023-09-13 01:02:54 +0530

    3

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-var tl =  gsap.timeline()
-tl.from(".nav img",{
-  
-    y:-100,
-    duration:0.5,
+var tl = gsap.timeline()
+// gsap.from("", {
 
-    scale:0.5
-   
-   })
-   tl.from(".navpart1,.navpart2",{
- 
-        y:-100,
-        duration:0.4,
-       
-        opacity:0,
-       stagger:1
-    
-   })
+//     y: -100,
+//     duration: 0.4,
+// delay: 0.3,
+//     scale: 0.5
+
+// })
+tl.from(".navpart1,.navpart2", {
+
+    y: -100,
+    duration: 0.5,
+    opacity: 0.4,
+    stagger: 1
+
+})
+tl.from(".heading h5 ,.heading h2,.heading h4 ,.heading button ", {
+
+    x: -100,
+    duration: 0.4,
+
+    opacity: 0,
+    stagger: 0.4
+
+})
