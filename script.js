2f2d9d68993786be49b9b2f9c318c3d0aebfa21e
adityap5 <aditya29pippal@gmail.com>
2023-09-13 00:13:45 +0530

    3

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ tl.from(".nav img",{
   
     y:-100,
     duration:0.5,
-  
+
     scale:0.5
    
    })
    tl.from(".navpart1,.navpart2",{
  
         y:-100,
-        duration:0.5,
+        duration:0.4,
        
         opacity:0,
        stagger:1
