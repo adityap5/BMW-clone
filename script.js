b62b475fc5582dfc77949adfaf15ada2a612207f
adityap5 <aditya29pippal@gmail.com>
2023-09-12 23:53:59 +0530

    2

---
+++ script.js
@@ -0,0 +1,18 @@
+var tl =  gsap.timeline()
+tl.from(".nav img",{
+  
+    y:-100,
+    duration:0.5,
+  
+    scale:0.5
+   
+   })
+   tl.from(".navpart1,.navpart2",{
+ 
+        y:-100,
+        duration:0.5,
+       
+        opacity:0,
+       stagger:1
+    
+   })
